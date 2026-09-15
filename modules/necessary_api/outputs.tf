6be4e635a23df280b113daec6e7288dcab58a28f
misafidiniaina <liantsoatsiorinirina@gmail.com>
2026-09-15 10:09:11 +0300

    fix(terraform): constrain versions and preserve project APIs

--- modules/necessary_api/outputs.tf
+++ modules/necessary_api/outputs.tf
@@ -5,6 +5,7 @@ output "enabled_services" {
     google_project_service.iam_api.service,
     google_project_service.container_api.service,
     google_project_service.cloudresourcemanager_api.service,
-    google_project_service.serviceusage_api.service
+    google_project_service.serviceusage_api.service,
+    google_project_service.artifact_registry_api.service
   ]
 }
---
+++ versions.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.9.0, < 2.0.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.12.0"
+    }
+  }
+}
